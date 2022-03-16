60bf76a960dd83e5e100f2a22255e2c63acaae58
Igor <machold.igor@gmail.com>
2022-03-16 16:33:49 +0100

    post

--- client/src/components/home/Home.jsx
+++ client/src/components/home/Home.jsx
@@ -4,11 +4,10 @@ import Navbar from "../navbar/Navbar";
 import React, { useEffect, useState } from 'react'
 import { useContext } from 'react'
 import { UserContext } from '../context'
-import { Link, useHistory } from "react-router-dom";
+import { useHistory } from "react-router-dom";
 import axios from "axios";
 
 export default function Home() {
-  const [showModal, setShowModal] = useState(false);
   const [newPost, setNewPost] = useState("");
   const [posts, setPosts] = useState([]);
 
@@ -24,7 +23,7 @@ export default function Home() {
     console.log("save post: response is", response);
 
     setNewPost("");
-    setShowModal(false);
+
 
     if (response.data.success) setPosts([...posts, response.data.post]);
   };
@@ -51,23 +50,19 @@ export default function Home() {
   },[]);
   return (
     <>
-      <div>Home</div>
      <div className="d-flex pt-4">
-      <Navbar />
+      <Navbar logout={handleLogout} />
       <div>
-        <Hoot />
+        <Hoot save={handleSave} onChange={(e) => setNewPost(e.target.value)} value={newPost}/>
       </div>
       
     
     </div>
 
 
+  
 
-
-      <button onClick={handleLogout}>Log out</button>
-      <button onClick={() => setShowModal(true)}>Add post</button>
-
-      {posts?.map((item) => (
+      {posts?.reverse().map((item) => (
         <div
           style={{
             border: "1px solid",
@@ -80,34 +75,9 @@ export default function Home() {
         </div>
       ))}
 
-      {showModal ? (
-        <Modal
-          save={handleSave}
-          close={() => setShowModal(false)}
-          value={newPost}
-          onChange={(e) => setNewPost(e.target.value)}
-        />
-      ) : (
-        <p>no posts available </p>
-      )}
+   
     </>
   );
 }
 
-function Modal({ save, close, value, onChange }) {
-  return (
-    <div
-      style={{
-        position: "fixed",
-        width: "1000px",
-        height: "300px",
-      }}
-    >
-      <textarea value={value} onChange={onChange} />
-      <p>
-        <button onClick={close}>Discard</button>
-        <button onClick={save}>Post</button>
-      </p>
-    </div>
-  );
-}
+
